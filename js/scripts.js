5bfdbcb2ce4cd58936ecf2c2c16d3bdc74a7bb65
Shruti Priya <shrutipriya1808@gmail.com>
2017-01-13 14:32:26 -0800

    minor changes in html javascript and jquery files

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    event.preventDefault();
+    var answer1 =$("input:radio[name=group]:checked").val();
+    var answer2 =$("input:radio[name=group]:checked").val();
+    var answer3 =$("input:radio[name=group]:checked").val();
+    var answer4 =$("input:radio[name=group]:checked").val();
+
+
+    if($("input:radio[name=group]:checked").val()===answerA===answerD")){
+      console.log(.results.option1)
+    }
